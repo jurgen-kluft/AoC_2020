e1ce444b729ccc44d0ec6383533cd9132b5a49d8
Jurgen Kluft <jurgen@virtuosgames.com>
2020-12-01 15:56:43 +0800

    * day1; sorted + binary search

--- source/puzzles/puzzle1.d
+++ source/puzzles/puzzle1.d
@@ -21,39 +21,57 @@ void solve_1_1()
         values ~= value;
     });
 
+    values.sort;
+    auto sorted = assumeSorted(values);
+
     int target = 2020;
 
-    for (int i=0; i<values.length; i++)
+    findsum: for (int i=0; i<values.length; i++)
     {
-        for (int j=i+1; j<values.length; j++)
+        int n1 = sorted[i];
+        int n2 = target - n1;
+        if (sorted.contains(n2))
         {
-            {
-                if ((values[i] + values[j]) == target)
-                {
-                    writeln("answer 1: ", values[i] * values[j]);
-                }
-            }
+            writeln("answer 1: ", n1 * n2);
+            break findsum;
         }
     }    
-
-    for (int i=0; i<values.length; i++)
-    {
-        for (int j=i+1; j<values.length; j++)
-        {
-            for (int k=j+1; k<values.length; k++)
-            {
-                if ((values[i] + values[j] + values[k]) == target)
-                {
-                    writeln("answer 2: ", values[i] * values[j] * values[k]);
-                }
-            }
-        }
-    }
-
 }
+
 
 /// 
 void solve_1_2()
 {
+    auto parser = new InputParser();
 
+    int[] values;
+    readFileLineByLine("input/input_1_1.text", (string line) {
+        //writeln(line); 
+        parser.reset(line);
+        int value;
+        parser.parse(value);
+        //writeln("  -> value=", value);
+        values ~= value;
+    });
+
+    values.sort;
+    auto sorted = assumeSorted(values);
+
+    int target = 2020;
+
+    findsum: for (int i=0; i<values.length; i++)
+    {
+        int n1 = sorted[i];
+        for (int j=i; j<values.length; j++)
+        {
+            int n2 = sorted[j];
+            int n3 = target - n1 - n2;
+            if (sorted.contains(n3))
+            {
+                writeln("answer 2: ", n1*n2*n3);
+                break findsum;
+            }
+        }
+    }    
 }
+
